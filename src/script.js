94639bc3006f9a0a754b630ce55c62df1abb45aa
HiroByteBit <116295469+HiroByteBit@users.noreply.github.com>
2024-11-21 00:48:10 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js';
+
 // Select the container element
 const container = document.getElementById('container');
 
